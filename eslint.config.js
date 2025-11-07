259b17b45f120e00b0146b4374fdffb00a5521a6
srunsronn <srorn.srun@student.cadt.edu.kh>
2025-11-07 14:25:18 +0700

    Initial Node.js project with test and lint

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    ignores: ["eslint.config.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+        ...globals.mocha,
+      },
+    },
+    rules: {
+ 
+    },
+  },
+];
---
+++ test/test.js
@@ -0,0 +1,11 @@
+import request from "supertest";
+import { expect } from "chai";
+import app from "../app.js";
+
+describe("GET /", () => {
+  it("should return Hello, GitHub Actions!", async () => {
+    const res = await request(app).get("/");
+    expect(res.status).to.equal(200);
+    expect(res.text).to.equal("Hello, GitHub Actions!");
+  });
+});
